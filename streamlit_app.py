40f97e6b0b0ac37faef615a638ae71559f6b88fb
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 10:20:21 +0300

    Update streamlit_app.py
    Add Another graph for YoY Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,30 +6,37 @@ from pmdarima import auto_arima
 st.title('North Cyprus CPI Forecast')
 
 # Load data
-df = pd.read_csv('inflation88seti.csv')
+df = pd.read_csv('inflation88seti.csv')  
 
 # Prepare datetime
 df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
 df.set_index('Date', inplace=True)
-df.index = df.index.to_period('M').to_timestamp() 
+df.index = df.index.to_period('M').to_timestamp()
 df.index.freq = 'MS'
 
 # Slider
-periods = st.slider('Periods', min_value=6, max_value=24, value=12)
+periods = st.slider('Periods', min_value=6, max_value=24, value=12)  
 
 # Model
 model = auto_arima(df['KKTC_CPI'])
 
 # Generate forecasts
 fc = model.predict(periods)
-fdf = pd.DataFrame(fc, columns=['Forecast']) 
-
-# Create forecast index
+fdf = pd.DataFrame(fc, columns=['Forecast'])
 fdf.index = pd.date_range(start=df.index[-1] + pd.DateOffset(months=1), periods=len(fc), freq='MS')
 
-# Plotly chart 
-fig = px.line(df, x=df.index, y='KKTC_CPI')
-fig.add_scatter(x=fdf.index, y=fdf['Forecast'], mode='lines', name='Forecast')
-fig.update_traces(hovertemplate='Date: %{x}<br>Value: %{y}')
+# CPI Chart
+fig1 = px.line(df, x=df.index, y='KKTC_CPI')
+fig1.update_layout(height=500) # Bigger size
+fig1.update_traces(hovertemplate='Date: %{x}<br>CPI: %{y}')
 
-st.plotly_chart(fig)
+# YoY Change  
+df['YoY Change'] = df['KKTC_CPI'].pct_change(periods=12) * 100
+
+fig2 = px.line(df, x=df.index, y='YoY Change')
+fig2.update_layout(height=500) # Bigger size 
+fig2.update_traces(hovertemplate='Date: %{x}<br>YoY Change: %{y:.2f}%')
+
+# Display charts
+st.plotly_chart(fig1) 
+st.plotly_chart(fig2)
