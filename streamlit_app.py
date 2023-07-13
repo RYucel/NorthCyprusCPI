aad968d7eba5883f59968454868475c0261e21d1
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 11:49:45 +0300

    Update streamlit_app.py
    Fix for confidence intervals by Claude 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,57 @@
-import pandas as pd 
+import pandas as pd
 import streamlit as st
-import plotly.express as px
+import plotly.graph_objects as go
 from pmdarima import auto_arima
 
 st.title('North Cyprus CPI Forecast')
 
-# Load data
-df = pd.read_csv('inflation88seti.csv')  
+# Load data 
+df = pd.read_csv('inflation88seti.csv')
 
-# Prepare datetime
-df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+# Prepare data
+df['Date'] = pd.to_datetime(df['Date'], dayfirst=True) 
 df.set_index('Date', inplace=True)
 df.index = df.index.to_period('M').to_timestamp()
-df.index.freq = 'MS'
-
-# Copy data
-forecast_df = df['KKTC_CPI'].to_frame('Forecast')
-
-# Slider
-periods = st.slider('Periods', min_value=6, max_value=24, value=12) 
+df.index.freq = 'MS'  
 
 # Model
 model = auto_arima(df['KKTC_CPI'])
 
 # Create figure
-fig = px.line()
+fig = go.Figure()
 
 # In-sample forecasts
 f, conf_int = model.predict_in_sample(return_conf_int=True)
 
 # Add in-sample ribbon
-fig.add_ribbon(x=conf_int.index, ymin=conf_int[:,0],  
-               ymax=conf_int[:,1], fillcolor='blue', opacity=0.2)
+fig.add_trace(go.Scatter(
+    x=conf_int.index, 
+    y=conf_int[:,0],
+    fill='tonexty', 
+    mode='lines',
+    line_color='blue'
+))
 
-# Out-of-sample forecasts  
+fig.add_trace(go.Scatter(
+    x=conf_int.index,
+    y=conf_int[:,1],
+    fill='tonexty',
+    mode='lines',
+    line_color='blue'  
+))
+
+# Out-of-sample forecasts
 fc = model.predict(periods)
 fdf = pd.DataFrame(fc, columns=['Forecast'])
-fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS')   
+fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS')
 
-# Add out-sample ribbon
-fig.add_ribbon(x=fdf.index, ymin=conf_int[:,0],
-               ymax=conf_int[:,1], fillcolor='green', opacity=0.2)
-
-# Plot forecast line
-fig.add_scatter(x=fdf.index, y=fdf['Forecast'], mode='lines', name='Forecast')
+# Add forecast line 
+fig.add_trace(go.Scatter(
+    x=fdf.index, 
+    y=fdf['Forecast'],
+    mode='lines',
+    name='Forecast'
+))
 
 # Display figure
 st.plotly_chart(fig)
