12b431436fbe831d7135fadc1feadcca1738803c
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 12:12:50 +0300

    Update streamlit_app.py
    Arrange display by claude

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,11 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from pmdarima import auto_arima
-from sklearn.metrics import mean_absolute_percentage_error
 
-st.title('North Cyprus CPI Forecast')
+st.title('North Cyprus CPI Forecast') 
 
 # Load data
-df = pd.read_csv('inflation88seti.csv')
+df = pd.read_csv('inflation88seti.csv')  
 
 # Prepare data
 df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
@@ -18,45 +17,39 @@ df.index.freq = 'MS'
 # Copy data
 forecast_df = df['KKTC_CPI'].to_frame('Forecast')
 
-# Slider
-periods = st.slider('Periods', min_value=6, max_value=24, value=12)
+# Smaller slider
+periods = st.slider('Periods', min_value=6, max_value=24, value=12, width=75)
 
 # Model
 model = auto_arima(df['KKTC_CPI'])
 
 # Forecasts
-fc = model.predict(periods)
+fc = model.predict(periods)  
 fdf = pd.DataFrame(fc, columns=['Forecast'])
-fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS')
+fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS') 
 
 # Concatenate
 forecast_df = pd.concat([forecast_df, fdf])
 
 # Calculate YoY
-forecast_df['YoY Change'] = forecast_df['Forecast'].pct_change(
-    periods=12) * 100
+forecast_df['YoY Change'] = forecast_df['Forecast'].pct_change(periods=12) * 100
 
 # Filter table
 yoy_table = forecast_df.loc[forecast_df.index >= '2020', 'YoY Change']
 
-# Sort table
+# Sort table  
 yoy_table = yoy_table.sort_index(ascending=False)
 
-# Plot 1
+# Plot 1 
 fig1 = px.line(forecast_df, x=forecast_df.index, y='Forecast')
 
 # Plot 2
 fig2 = px.line(forecast_df, x=forecast_df.index, y='YoY Change')
 
-# Model metric
-smape = mean_absolute_percentage_error(
-    df['KKTC_CPI'], model.predict_in_sample())
-st.markdown(f"**SMAPE:** {smape:.2%}")
-
-# Display plots
-st.plotly_chart(fig1)
-st.plotly_chart(fig2)
+# Arrange plots
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig1, use_container_width=True)
+right_column.plotly_chart(fig2, use_container_width=True)
 
 # Display table
 st.table(yoy_table.round(2))
-# python -m streamlit run app.py --server.port 8502
