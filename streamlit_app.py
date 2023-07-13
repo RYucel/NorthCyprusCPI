b4a7e791052917b96da3054b7908498f62c0acbc
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 12:05:04 +0300

    Update streamlit_app.py
    Revert back to working one with table and yoy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,107 +1,62 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import plotly.graph_objects as go
 from pmdarima import auto_arima
+from sklearn.metrics import mean_absolute_percentage_error
 
 st.title('North Cyprus CPI Forecast')
 
 # Load data
-df = pd.read_csv('inflation88seti.csv') 
+df = pd.read_csv('inflation88seti.csv')
 
-# Prepare datetime
+# Prepare data
 df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
 df.set_index('Date', inplace=True)
 df.index = df.index.to_period('M').to_timestamp()
 df.index.freq = 'MS'
 
-# Copy data  
+# Copy data
 forecast_df = df['KKTC_CPI'].to_frame('Forecast')
+
+# Slider
+periods = st.slider('Periods', min_value=6, max_value=24, value=12)
 
 # Model
 model = auto_arima(df['KKTC_CPI'])
 
-# Figure 
-fig = go.Figure()  
-
-# In-sample forecasts
-f, conf_int = model.predict_in_sample(return_conf_int=True)
-
-# Get original forecast index
-fcst_index = df.index  
-
-# Add in-sample ribbon
-fig.add_trace(go.Scatter(
-    x=fcst_index,
-    y=conf_int[:,0],
-    fill='tonexty',
-    mode='lines',
-    line_color='green'))
-
-fig.add_trace(go.Scatter(
-    x=fcst_index,
-    y=conf_int[:,1],
-    fill='tonexty', 
-    mode='lines',
-    line_color='blue'))
-    
-# Out-of-sample forecasts
-periods = 12
-fc, conf_int = model.predict(periods, return_conf_int=True)  
+# Forecasts
+fc = model.predict(periods)
 fdf = pd.DataFrame(fc, columns=['Forecast'])
 fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS')
 
-# Add out-sample ribbon 
-fig.add_trace(go.Scatter(
-    x=fdf.index,
-    y=conf_int[:,0],
-    fill='tonexty', 
-    mode='lines',
-    line_color='green')) 
-    
-fig.add_trace(go.Scatter(
-    x=fcst_index,
-    y=conf_int[:,1],
-    fill='tonexty',
-    mode='lines',
-    line_color='blue'))
-    
-# Out-of-sample forecasts
-periods = 12    
-fc, conf_int = model.predict(periods, return_conf_int=True)
-fdf = pd.DataFrame(fc, columns=['Forecast'])
-fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS')
+# Concatenate
+forecast_df = pd.concat([forecast_df, fdf])
 
-# Add out-sample ribbon
-fig.add_trace(go.Scatter(
-    x=fdf.index,
-    y=conf_int[:,0],
-    fill='tonexty',  
-    mode='lines',
-    line_color='green'))
+# Calculate YoY
+forecast_df['YoY Change'] = forecast_df['Forecast'].pct_change(
+    periods=12) * 100
 
-fig.add_trace(go.Scatter(
-    x=fdf.index,
-    y=conf_int[:,1],
-    fill='tonexty',
-    mode='lines',
-    line_color='green'))
-    
-# Add forecast line
-fig.add_trace(go.Scatter(
-    x=fdf.index,
-    y=fdf['Forecast'],
-    mode='lines',
-    name='Forecast'))
-    
-# Second plot 
-fig2 = px.line(forecast_df, x=forecast_df.index, y='YoY Change')
-
-# Filtered YoY table
+# Filter table
 yoy_table = forecast_df.loc[forecast_df.index >= '2020', 'YoY Change']
+
+# Sort table
 yoy_table = yoy_table.sort_index(ascending=False)
 
-# Display plots and table  
-st.plotly_chart(fig)
-st.plotly_chart(fig2) 
+# Plot 1
+fig1 = px.line(forecast_df, x=forecast_df.index, y='Forecast')
+
+# Plot 2
+fig2 = px.line(forecast_df, x=forecast_df.index, y='YoY Change')
+
+# Model metric
+smape = mean_absolute_percentage_error(
+    df['KKTC_CPI'], model.predict_in_sample())
+st.markdown(f"**SMAPE:** {smape:.2%}")
+
+# Display plots
+st.plotly_chart(fig1)
+st.plotly_chart(fig2)
+
+# Display table
 st.table(yoy_table.round(2))
+# python -m streamlit run app.py --server.port 8502
