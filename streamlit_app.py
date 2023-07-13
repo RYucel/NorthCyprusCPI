f10f2b1032301852489cbd1b457eac133f56ca9e
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 11:04:36 +0300

    Update streamlit_app.py
    Revert back to working one Claude

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from pmdarima import auto_arima
 st.title('North Cyprus CPI Forecast')
 
 # Load data
-df = pd.read_csv('inflation88seti.csv')  
+df = pd.read_csv('inflation88seti.csv')
 
 # Prepare datetime
 df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
@@ -17,34 +17,21 @@ df.index.freq = 'MS'
 # Slider
 periods = st.slider('Periods', min_value=6, max_value=24, value=12)
 
-# Model  
+# Model
 model = auto_arima(df['KKTC_CPI'])
 
-# Forecasts
+# Generate forecasts
 fc = model.predict(periods)
-fc_df = pd.DataFrame(fc, columns=['KKTC_CPI']) 
+fdf = pd.DataFrame(fc, columns=['Forecast'])
 
-fdf = pd.DataFrame(fc, columns=['KKTC_CPI'])
-fdf.index = pd.date_range(start=df.index[-1] + pd.DateOffset(months=1), periods=len(fc), freq='MS')
+# Create forecast index
+fdf.index = pd.date_range(
+    start=df.index[-1] + pd.DateOffset(months=1), periods=len(fc), freq='MS')
 
-# Add forecast column to fdf
-fdf = pd.concat([fdf, fc_df['KKTC_CPI']], axis=1)
-fdf.rename(columns={'KKTC_CPI': 'Forecast'}, inplace=True)
+# Plotly chart
+fig = px.line(df, x=df.index, y='KKTC_CPI')
+fig.add_scatter(x=fdf.index, y=fdf['Forecast'], mode='lines', name='Forecast')
+fig.update_traces(hovertemplate='Date: %{x}<br>Value: %{y}')
 
-# YoY Forecast
-fdf['YoY Forecast'] = df['KKTC_CPI'].pct_change(periods=12) * 100
-
-# CPI Chart
-fig1 = px.line(df, x=df.index, y='KKTC_CPI', color='blue', label='Actual')
-fig1.add_scatter(x=fdf.index, y=fdf['Forecast'], mode='lines', name='Forecast', color='red')
-fig1.update_traces(hovertemplate='Date: %{x}<br>CPI: %{y}') 
-
-# YoY Change  
-df['YoY Change'] = df['KKTC_CPI'].pct_change(periods=12) * 100
-
-fig2 = px.line(df, x=df.index, y='YoY Change', color='blue', label='Actual')
-fig2.add_scatter(x=fdf.index, y=fdf['YoY Forecast'], mode='lines', name='Forecast', color='red')
-fig2.update_traces(hovertemplate='Date: %{x}<br>YoY Change: %{y:.2f}%')
-
-st.plotly_chart(fig1)
-st.plotly_chart(fig2)
+st.plotly_chart(fig)
+# python -m streamlit run app.py --server.port 8502
