494f70c6b871306ec4c577c5d33a900bc57ca0be
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 08:26:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 st.title('North Cyprus CPI Forecast')
 
 # Load data
-df = pd.read_csv('data.csv') 
+df = pd.read_csv('inflation88seti.csv') 
 
 # Train autoARIMA model
-model = auto_arima(df['NorthCyprusCPI'], start_p=1, start_q=1, max_p=3, max_q=3, m=12, start_P=0, seasonal=True, d=1, D=1, trace=True, error_action='ignore', suppress_warnings=True)
+model = auto_arima(df['KKTC_CPI'], start_p=1, start_q=1, max_p=3, max_q=3, m=12, start_P=0, seasonal=True, d=1, D=1, trace=True, error_action='ignore', suppress_warnings=True)
 
 # Forecast into future
 n_periods = 24
