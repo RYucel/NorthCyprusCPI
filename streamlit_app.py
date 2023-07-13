4bf58ebf94aec4f9d4898935e49d5405f8dcc11e
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 11:25:17 +0300

    Update streamlit_app.py
    Fix for YoY tabel by Claude 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,35 +9,34 @@ st.title('North Cyprus CPI Forecast')
 df = pd.read_csv('inflation88seti.csv')
 
 # Prepare datetime
-df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)  
+df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
 df.set_index('Date', inplace=True)
 df.index = df.index.to_period('M').to_timestamp()
 df.index.freq = 'MS'
 
-# Copy actual data
-df['Forecast'] = df['KKTC_CPI']  
+# Copy actual data 
+forecast_df = df['KKTC_CPI'].to_frame('Forecast')
 
 # Slider
-periods = st.slider('Periods', min_value=6, max_value=24, value=12)
+periods = st.slider('Periods', min_value=6, max_value=24, value=12)  
 
 # Model
-model = auto_arima(df['KKTC_CPI'])  
+model = auto_arima(df['KKTC_CPI'])
 
 # Forecasts
-fc = model.predict(periods)
+fc = model.predict(periods)  
 fdf = pd.DataFrame(fc, columns=['Forecast'])
-fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS') 
+fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS')
 
-# Append forecasts  
-df['Forecast'] = df['Forecast'].append(fdf['Forecast'])
-df = df.append(fdf)
+# Append forecasts
+forecast_df = forecast_df.append(fdf)
 
-# Plot
-fig = px.line(df, x=df.index, y='Forecast')
+# Plot 
+fig = px.line(forecast_df, x=forecast_df.index, y='Forecast')
 st.plotly_chart(fig)
 
-# Calculate YoY  
-df['YoY Change'] = df['Forecast'].pct_change(periods=12) * 100
+# Calculate YoY
+forecast_df['YoY Change'] = forecast_df['Forecast'].pct_change(periods=12) * 100  
 
 # Display table
-st.table(df['YoY Change'].round(2))
+st.table(forecast_df['YoY Change'].round(2))
