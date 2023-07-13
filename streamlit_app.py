b4fa44b93172b17cc1db9a93e768d3b25c05c9ad
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 10:10:20 +0300

    Update streamlit_app.py
    Turned all to functions by Claude

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,51 +3,41 @@ import streamlit as st
 import plotly.express as px
 from pmdarima import auto_arima
 
+# Load and prepare data
+@st.cache
+def load_data():
+    df = pd.read_csv("data.csv")
+    df["Date"] = pd.to_datetime(df["Date"]) 
+    df.set_index("Date", inplace=True)
+    df.index = df.index.to_period("M").to_timestamp()
+    df.index.freq = "MS"
+    return df
 
-st.title('North Cyprus CPI Forecast')
+df = load_data()
 
-# Initialize session state
-if "slider" not in st.session_state:
-    st.session_state["slider"] = None
+# Create model
+@st.cache
+def create_model(df):
+    model = auto_arima(df["KKTC_CPI"])
+    return model
 
-# Load data  
-df = pd.read_csv('inflation88seti.csv')
+model = create_model(df)
 
-# Prepare dataframe
-df['Date'] = pd.to_datetime(df['Date'], dayfirst=True) 
-df.set_index('Date', inplace=True)
-df.index = df.index.to_period('M').to_timestamp()
-df.index.freq = 'MS'
-df = df.copy()
+# Generate forecasts
+def generate_forecasts(model, periods):
+   fc = model.predict(periods)
+   fdf = pd.DataFrame(fc, columns=["Forecast"])
+   fdf.index = pd.date_range(start=df.index[-1] + pd.DateOffset(months=1), periods=len(fc), freq="MS")
+   return fdf
 
-# Slider
-periods = st.slider('Periods', min_value=6, max_value=24, value=12)  
-
-# Model
-model = auto_arima(df['KKTC_CPI'])
-
-# Plotly figure
-fig = px.line(df, x=df.index, y='KKTC_CPI')
-
-# Add forecast
-# Forecasts
-fc = model.predict(periods)
-fdf = pd.DataFrame(fc, columns=['Forecast'])
-
-# Generate forecast index
-fdf.index = pd.date_range(start=df.index[-1] + pd.DateOffset(months=1), periods=len(fc), freq='MS') 
-
-# Plot
-fig.add_scatter(x=fdf.index, y=fdf['Forecast'], mode='lines', name='Forecast')
-
-# Tooltips
-fig.update_traces(hovertemplate='Date: %{x}<br>Value: %{y}')
-
-st.plotly_chart(fig)
-# Initial call
-plot_forecast()  
-
-# Register on change callback
-st.session_state["slider"].on_change(plot_forecast)
-
-st.pyplot(fig)
+# Plotly chart
+def plot_chart(df, fdf):
+    fig = px.line(df, x=df.index, y="KKTC_CPI") 
+    fig.add_scatter(x=fdf.index, y=fdf["Forecast"], mode="lines", name="Forecast")
+    fig.update_traces(hovertemplate="Date: %{x}<br>Value: %{y}")
+    st.plotly_chart(fig)
+    
+# Run app
+periods = st.slider("Periods", min_value=6, max_value=24, value=12)
+fdf = generate_forecasts(model, periods) 
+plot_chart(df, fdf)
