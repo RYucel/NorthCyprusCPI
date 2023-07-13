d50a52221f861e031d9a421adee09a514dd97411
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 12:00:42 +0300

    Update streamlit_app.py
    Fix confidence by Claude 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from pmdarima import auto_arima
 st.title('North Cyprus CPI Forecast')
 
 # Load data
-df = pd.read_csv('inflation88seti.csv')
+df = pd.read_csv('inflation88seti.csv') 
 
 # Prepare datetime
 df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
@@ -14,20 +14,20 @@ df.set_index('Date', inplace=True)
 df.index = df.index.to_period('M').to_timestamp()
 df.index.freq = 'MS'
 
-# Copy data
+# Copy data  
 forecast_df = df['KKTC_CPI'].to_frame('Forecast')
 
-# Model 
+# Model
 model = auto_arima(df['KKTC_CPI'])
 
-# Figure
-fig = go.Figure()
+# Figure 
+fig = go.Figure()  
 
 # In-sample forecasts
 f, conf_int = model.predict_in_sample(return_conf_int=True)
 
-# Get forecast index
-fcst_index = f.index
+# Get original forecast index
+fcst_index = df.index  
 
 # Add in-sample ribbon
 fig.add_trace(go.Scatter(
@@ -35,7 +35,28 @@ fig.add_trace(go.Scatter(
     y=conf_int[:,0],
     fill='tonexty',
     mode='lines',
+    line_color='green'))
+
+fig.add_trace(go.Scatter(
+    x=fcst_index,
+    y=conf_int[:,1],
+    fill='tonexty', 
+    mode='lines',
     line_color='blue'))
+    
+# Out-of-sample forecasts
+periods = 12
+fc, conf_int = model.predict(periods, return_conf_int=True)  
+fdf = pd.DataFrame(fc, columns=['Forecast'])
+fdf.index = pd.date_range(start=df.index[-1], periods=len(fc), freq='MS')
+
+# Add out-sample ribbon 
+fig.add_trace(go.Scatter(
+    x=fdf.index,
+    y=conf_int[:,0],
+    fill='tonexty', 
+    mode='lines',
+    line_color='green')) 
     
 fig.add_trace(go.Scatter(
     x=fcst_index,
