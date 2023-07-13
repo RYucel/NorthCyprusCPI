1ffb9a28d765288a47f62b2e18679639ed493113
Rustu YUCEL <rustuyucel@gmail.com>
2023-07-13 10:13:04 +0300

    Update streamlit_app.py
    Revert to original format of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,41 +3,33 @@ import streamlit as st
 import plotly.express as px
 from pmdarima import auto_arima
 
-# Load and prepare data
-@st.cache
-def load_data():
-    df = pd.read_csv("data.csv")
-    df["Date"] = pd.to_datetime(df["Date"]) 
-    df.set_index("Date", inplace=True)
-    df.index = df.index.to_period("M").to_timestamp()
-    df.index.freq = "MS"
-    return df
+st.title('North Cyprus CPI Forecast')
 
-df = load_data()
+# Load data
+df = pd.read_csv('inflation88seti.csv')
 
-# Create model
-@st.cache
-def create_model(df):
-    model = auto_arima(df["KKTC_CPI"])
-    return model
+# Prepare datetime
+df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+df.set_index('Date', inplace=True)
+df.index = df.index.to_period('M').to_timestamp() 
+df.index.freq = 'MS'
 
-model = create_model(df)
+# Slider
+periods = st.slider('Periods', min_value=6, max_value=24, value=12)
+
+# Model
+model = auto_arima(df['KKTC_CPI'])
 
 # Generate forecasts
-def generate_forecasts(model, periods):
-   fc = model.predict(periods)
-   fdf = pd.DataFrame(fc, columns=["Forecast"])
-   fdf.index = pd.date_range(start=df.index[-1] + pd.DateOffset(months=1), periods=len(fc), freq="MS")
-   return fdf
+fc = model.predict(periods)
+fdf = pd.DataFrame(fc, columns=['Forecast']) 
 
-# Plotly chart
-def plot_chart(df, fdf):
-    fig = px.line(df, x=df.index, y="KKTC_CPI") 
-    fig.add_scatter(x=fdf.index, y=fdf["Forecast"], mode="lines", name="Forecast")
-    fig.update_traces(hovertemplate="Date: %{x}<br>Value: %{y}")
-    st.plotly_chart(fig)
-    
-# Run app
-periods = st.slider("Periods", min_value=6, max_value=24, value=12)
-fdf = generate_forecasts(model, periods) 
-plot_chart(df, fdf)
+# Create forecast index
+fdf.index = pd.date_range(start=df.index[-1] + pd.DateOffset(months=1), periods=len(fc), freq='MS')
+
+# Plotly chart 
+fig = px.line(df, x=df.index, y='KKTC_CPI')
+fig.add_scatter(x=fdf.index, y=fdf['Forecast'], mode='lines', name='Forecast')
+fig.update_traces(hovertemplate='Date: %{x}<br>Value: %{y}')
+
+st.plotly_chart(fig)
